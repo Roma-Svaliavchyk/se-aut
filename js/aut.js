162976f8e2f8509e1cec5f86219df97ae0dd5fcb
RomanSvalyavchyk <c.svalyavchyk.roman@student.uzhnu.edu.ua>
2024-06-17 06:04:30 +0300

    e4

--- js/aut.js
+++ js/aut.js
@@ -32,6 +32,18 @@ function handleSubmit(event) {
       containerError.classList.add('list-error-tue');
       containerError.innerHTML =`<li class="error-message">Авторизація успішна!</li>`;
 
+      const lUserData = {email: emailInput.value, token: response.data.token };    
+
+      axios
+      .patch("https://solar-energy-serv.onrender.com/luser", lUserData)
+      .then((response) => {
+        console.log("Updated data: ", response.data);
+      })
+      .catch((error) => {
+        console.error("Error updating user: ", error);
+      });
+
+
     }catch(err){
       console.log("error 1")
       console.log(err);
